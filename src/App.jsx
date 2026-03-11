8b03707e4535f4da9d064d5318f26f2ebd7c6e0e
Cevdet Baran Oral <cevdetbaranoral@gmail.com>
2026-03-12 00:03:17 +0200

    feat: Implement Google Analytics tracking for page views

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import AIDemos from "./pages/AIDemos";
 import NotFound from "./pages/NotFound";
+import { initAnalytics, trackPageView } from "./utils/analytics";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    initAnalytics();
+
+    const path = `${location.pathname}${location.search}${location.hash}`;
+    trackPageView(path, document.title);
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <AnalyticsTracker />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
